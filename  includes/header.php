110c74ddacf569e6ac5d6c3d38b3864b6bd81751
bestdevelope <leabouillot7@gmail.com>
2024-09-01 20:13:29 +0200

    images

---
+++  includes/header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+        <a href="index.html" class="lien-icone">
+            <img class="logo" src="images/logo-atelir.png" alt="Logo atelier" />
+        </a>
+        <div>
+            <a href="index.html">Home</a>
+            <a href="about.html">Publier Article</a>
+            <a href="portfolio.html">Login</a>
+            <input class="login" id="login-input" type="text" placeholder="Votre nom" />
+            <input class="submit" type="submit" value="Se connecter" />
+            <h4 id="salutation" class="hidden"></h4>
+        </div>
+</header>
--- Views/home.php
+++ Views/home.php
@@ -8,7 +8,34 @@
 </head>
 
 <body>
+    <?php include_once '../includes/header.php'; ?>
+    <section>
+        <div>
+            <h2> Welcome to my blog</h2>
+            <p>
+                Où <em>professionalisme</em> s’allie avec <em>passion</em>. Depuis
+                plus de 5 ans maintenant, j’exerce mon métier avec la passion qui
+                m’anime : capturer l’essence de chaque instant.
+            </p>
+            <a href="#contact" class="cta">UN PROJET ? ÉCRIVEZ-MOI</a>
+        </div>
+    </section>
+    <div>
+        <h2>Suivez l'actualité</h2>
+        <?php foreach ($carticles as $article): ?>
+            <div>
+                <h3><?= $article['title'] ?></h3>
+                <p>Écrit par <?= $article['author'] ?></p>
+                <p><?= $article['resume'] ?></p>
+                <p>Publié le <?= $article['date'] ?></p>
+                <a href="./publier.php">En lire plus</a>
+            </div>
 
+        <?php endforeach; ?>
+
+    </div>
+
+    <?php include_once '../includes/footer.php'; ?>
 </body>
 
 </html>
