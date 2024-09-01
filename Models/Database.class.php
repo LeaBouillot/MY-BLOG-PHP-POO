b5b9bf2f5b4053c5ad3d93e81864b36acad3d86e
bestdevelope <leabouillot7@gmail.com>
2024-09-01 12:12:22 +0200

    creation les fiches

---
+++ Models/Database.class.php
@@ -0,0 +1,11 @@
+<?php
+
+abstract class Database
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = new PDO("mysql:host=localhost;dbname=blog;charset=utf8", 'root', '');
+    }
+}
